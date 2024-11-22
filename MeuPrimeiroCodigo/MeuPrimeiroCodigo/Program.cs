cd9c10a082d270c1bbaa2730315d4239407c4653
Guilherme Oliveira <guilhermedepaula1623@gmail.com>
2024-11-21 22:13:07 -0300

    Importado o arquivo do código feito no vscode.

---
+++ MeuPrimeiroCodigo/MeuPrimeiroCodigo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write é para escrever para o usuario
+            Console.Write("Hello World");
+            
+            // Comando usado para ele fechar só quando apertarem uma tecla
+            Console.ReadKey();
+
+        }
+    }
+}
